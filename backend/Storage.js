a7bd9747eb9a341fd875b5429acd56f551bffaaa
Steven Ramkumar <steveram@gmail.com>
2010-12-30 21:28:14 -0800

    add functionality to show the most active rooms on the homepage and to join them

--- backend/Storage.js
+++ backend/Storage.js
@@ -8,15 +8,43 @@ var DB_NAME = 'uwchat',
     DB_ADDESS = '127.0.0.1',
     DB_PORT = 27017;
 
+/**
+ * Inserts document into mongodb
+ * @param {String} coll_name name of the collection in the database
+ * @param {Object} data an object literal to insert into the database
+ */
 Storage.save = function(coll_name, data) {
-  var client = new Db(DB_NAME, new Server(DB_ADDESS, DB_PORT));
+    var client = new Db(DB_NAME, new Server(DB_ADDESS, DB_PORT));
 
-  client.open(function(p_client) {
-    client.collection(coll_name, function(err, collection) {
-      collection.insert(data, function(err, docs) {
-        sys.puts('log message ' + JSON.stringify(data) + '.');
-        client.close();
-      });
+    client.open(function(p_client) {
+        client.collection(coll_name, function(err, collection) {
+            collection.insert(data, function(err, docs) {
+                sys.puts('log message ' + JSON.stringify(data) + '.');
+                client.close();
+            });
+        });
     });
-  });
+}
+
+/**
+ * Gets the most active rooms on UWChat. Does a SQL-like GROUP BY
+ * query to get the number of messages in the last half hour in each
+ * existing chat room.
+ * @param {String} coll_name name of the collection in the database
+ * @param {Object} data an object literal to insert into the database
+ */
+Storage.getActiveRooms = function(coll_name, callback){
+    var client = new Db(DB_NAME, new Server(DB_ADDESS, DB_PORT));
+    
+    var SINCE = [300000, 600000, 1800000]; //5, 10, 30 minutes
+    
+    client.open(function(p_client) {
+        client.collection(coll_name, function(err, collection) {
+            collection.group(['room'], {type: 'msg', timestamp: {$gt : new Date().getTime() - SINCE[2]}}, {sum:0}, function(doc, prev){ prev.sum += 1}, function(err, docs) {
+                sys.puts('log message ' + JSON.stringify(docs) + '.');
+                callback(docs);
+                client.close();
+            });
+        });
+    });
 }
--- frontend/js/chat_client.js
+++ frontend/js/chat_client.js
@@ -276,6 +276,34 @@ function updateUptime () {
   }
 }
 
+function updateActiveRooms (data) {
+    var topRoomsTable = $("#activerooms_splash"),
+        room,
+        content = '';
+    
+    //mongodb-native doesn't have a sort function after a
+    // group query. So we have to sort client side.
+    data.sort(function(a,b){ return b.sum - a.sum});
+    
+    // What about using Mustache templates instead?
+    data.forEach(function(obj){
+        room = obj.room.length > 21 ? obj.room.substr(0, 15).toLowerCase() + "..." : obj.room.toLowerCase();
+        
+        content += '<div><div class = "room_name">' + room + '</div>' 
+                    + '<div class = "join_button_div"><a href = "#" class = "button join_button" data-action = "'+obj.room.toLowerCase()+'">Join</a></div></div>';
+    });
+    
+    topRoomsTable.append(content);
+    
+    $("#activerooms_splash .join_button").click(function(e){
+        //Should probably abstract this out later.
+        var roomName = $(this).attr("data-action");
+        $("#nickInput").attr("value", roomName);
+        $("#connectButton").click();
+    });
+           
+}
+
 var transmission_errors = 0;
 var first_poll = true;
 
@@ -420,8 +448,15 @@ function showChat (nick) {
 
   $("#loading").hide();
   $("#example").hide();
+  $("#activerooms_splash").hide();
 
   scrollDown();
+}
+
+function showActiveRooms () {
+    jQuery.get("/activerooms", {}, function (data) {
+        updateActiveRooms(eval("(" + data + ")"));
+    });
 }
 
 //we want to show a count of unread messages when the window does not have focus
@@ -590,6 +625,7 @@ $(document).ready(function() {
   $("#log table").remove();
 
   showConnect();
+  showActiveRooms();
 });
 
 //if we can, notify the server that we're going away.
